e8c0638aba481fd02ea3cc21a15f18c6550e63dc
alisher <chexramchik@gmail.com>
2022-07-22 17:55:29 +0500

    dz

--- script.js
+++ script.js
@@ -63,44 +63,10 @@ setTimeout(() => {
 }, 500);
  }
 
-
-
-
-
-
-// let  acc = document.getElementsByClassName("accordion");
-// let  i;
-
-// for (i = 0; i < acc.length; i++) {
-//   acc[i].addEventListener("click", function() {
-//     this.classList.toggle("active");
-
-//     let panel = this.nextElementSibling;
-//     if (panel.style.display === "block") {
-//       panel.style.display = "none";
-//     } else {
-//       panel.style.display = "block";
-//     }
-//   });
-// }
-
-
-
-
-
 const accordionItemHeaders = document.querySelectorAll(".accordion-item-header");
 
 accordionItemHeaders.forEach(accordionItemHeader => {
    accordionItemHeader.addEventListener("click", event => {
-    
-     // Uncomment in case you only want to allow for the display of only one collapsed item at a time!
-    
-//     const currentlyActiveAccordionItemHeader = document.querySelector(".accordion-item-header.active");
-//     if(currentlyActiveAccordionItemHeader && currentlyActiveAccordionItemHeader!==accordionItemHeader) {
-//        currentlyActiveAccordionItemHeader.classList.toggle("active");
-//        currentlyActiveAccordionItemHeader.nextElementSibling.style.maxHeight = 0;
-//      }
-
      accordionItemHeader.classList.toggle("active");
      const accordionItemBody = accordionItemHeader.nextElementSibling;
      if(accordionItemHeader.classList.contains("active")) {
